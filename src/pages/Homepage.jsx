0fd2996184132db4779f97b9c774851ea47ba595
Ryan Fleming <ryanfleming93@aol.com>
2024-11-11 14:51:03 +0000

    added bitcoin wallet

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
+import { QRCodeCanvas } from "qrcode.react"; // Import QR Code
+import toolIcons from "../utils/toolIcons";
 
 const Homepage = () => {
-  const navigate = useNavigate();
   const [blurred, setBlurred] = useState(true);
+  const navigate = useNavigate();
+  const walletAddress = "yourwallet.crypto"; // Replace with your wallet address
 
   return (
     <div
@@ -27,27 +30,27 @@ const Homepage = () => {
         }}
       />
 
-      {/* Main Content (Navbar + other content) */}
+      {/* Main Content */}
       <div
         className={`relative flex flex-col min-h-screen`}
         style={{
-          zIndex: 2 // Ensure text is above the overlay
+          zIndex: 2
         }}
       >
         <Navbar />
 
-        {/* Main Section - Centered Under Navbar */}
+        {/* Main Section */}
         <div className="flex-grow flex flex-col items-left h-full p-8 pt-32 sm:p-12 sm:justify-center sm:pb-24 md:justify-center md:p-24 max-w-full container mx-auto">
           <h1 className="drop-shadow-md text-white font-bold text-6xl sm:text-7xl md:text-9xl mb-4 -ml-1 sm:-ml-2 text-center md:text-left lg:text-left">
             Ryan Fleming
           </h1>
 
-          {/* Additional Content Section */}
           <div className="text-white text-lg sm:text-xl text-center max-w-md md:text-left lg:text-left ml-0">
             <p>
               I specialize in web development, marketing tools, AI, game
               development, 3D modeling, and all things web.
             </p>
+
             <button
               onClick={() => navigate("/projects")}
               style={{
@@ -60,11 +63,28 @@ const Homepage = () => {
                 fontSize: "18px",
                 filter:
                   "drop-shadow(0 4px 3px rgba(0, 0, 0, 0.07)) drop-shadow(0 2px 2px rgba(0, 0, 0, 0.06))",
-                margin: "24px 0 0 0" // Replace the semicolon here with a comma
+                margin: "24px 0 0 0"
               }}
             >
               View Projects
             </button>
+            {/* Bitcoin Wallet Section */}
+            <div className="flex w-full items-center justify-start space-x-4 mb-6 py-12 rounded-lg ">
+              {/* QR Code */}
+              <div className="bg-white p-1 rounded-md">
+                <QRCodeCanvas
+                  value={`bitcoin:${walletAddress}`}
+                  size={60} // Compact size
+                  bgColor={"#ffffff"}
+                  fgColor={"#000000"}
+                  includeMargin={false} // No extra margin for compactness
+                />
+              </div>
+              {/* Bitcoin Address */}
+              <p className="text-white font-mono text-sm px-2 py-1 rounded-md break-words">
+                I collect Bitcoin
+              </p>
+            </div>
           </div>
         </div>
 
@@ -91,7 +111,7 @@ const Homepage = () => {
 
       {/* Blur Layer */}
       {blurred && (
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center bg-black bg-opacity-20 backdrop-blur-lg transition-opacity ">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center bg-black bg-opacity-20 backdrop-blur-lg transition-opacity">
           <button
             onClick={() => setBlurred(false)}
             className="text-white font-bold text-7xl z-20"
